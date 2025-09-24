e18f9eb6d786164f8117c896bc929e102b8e2725
juan <jporta.09@gmail.com>
2025-09-23 23:44:32 -0300

    added automation, mail and pdf templates

--- road_union_economic_management/__manifest__.py
+++ road_union_economic_management/__manifest__.py
@@ -9,13 +9,14 @@
     'category': 'Union',
     'version': '1.0',
     "license": "AGPL-3",
-    'depends': ['base', 'union_affiliation', 'road_union_affiliation'],
+    'depends': ['base', 'base_automation', 'union_affiliation', 'road_union_affiliation'],
     'data': [
         'views/payment_account_current_views.xml',
         'security/ir.model.access.csv',
         'views/affiliate_views.xml',
         'wizards/payment_account_filter.xml',
         'wizards/meta_4_download.xml',
+        'views/reports.xml',
     ],
     "installable": True,
 }
